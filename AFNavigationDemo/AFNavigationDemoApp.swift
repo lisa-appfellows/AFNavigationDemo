4fd74c50a81f0e02bca979bd70e8013fd3564b7e
Lisa Fellows <100980689+lisa-appfellows@users.noreply.github.com>
2026-08-15 12:11:13 -0600

    chore: project setup - .gitignore, .ci.yml, .cd.yml

---
+++ AFNavigationDemo/AFNavigationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFNavigationDemoApp.swift
+//  AFNavigationDemo
+//
+//  Created by Lisa Fellows on 2026-08-15.
+//
+
+import SwiftUI
+
+@main
+struct AFNavigationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
